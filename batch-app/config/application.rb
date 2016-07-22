01857b4db49c86146a19201bb933adf49d29f2b1
John Anderson <panic@semiosix.com>
2016-07-22 09:57:33 +0200

    add batch torquebox initialisation

--- batch-app/config/application.rb
+++ batch-app/config/application.rb
@@ -17,6 +17,11 @@ Bundler.require(*Rails.groups)
 
 module BatchApp
   class Application < Rails::Application
+      # Use TorqueBox::Infinispan::Cache for the Rails cache store
+  if defined? TorqueBox::Infinispan::Cache
+    config.cache_store = :torquebox_store
+  end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
